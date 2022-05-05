3e32eee4d02086324847d7e12bfccf61e29a42f5
antoine <antoine-haller@outlook.fr>
2022-05-05 14:00:26 +0200

    quotes

--- socket/src/Controller/GameController.js
+++ socket/src/Controller/GameController.js
@@ -1,25 +1,24 @@
 const server = require('../../server')
 const localStorage = require('../Services/LocalStore/LocalStore')
 
-module.exports.StartingGame = function () {
-  // here notified by mobile app for start game
-  /**
-   * the truc qui fait que quand on envoie vrai ou faut on ouvre les ports
-   * et stocker les ports
-   */
-  var data = {
-    users: [{
-      0: {
-        'name' : "Lexo"
-      },
-      1: {
-        'name' : "ANTOINE LE MEC TROP BEAU JE SUIS SUR TU SUCE MIEUX QUE MON EX"
-      },
+module.exports.StartingGame = function() {
+    // here notified by mobile app for start game
+    /**
+     * the truc qui fait que quand on envoie vrai ou faut on ouvre les ports
+     * et stocker les ports
+     */
+    var data = {
+        users: [{
+            0: {
+                'name': "Lexo"
+            },
+            1: {
+                'name': "ANTOINE LE MEC TROP BEAU JE SUIS SUR TU SUCE MIEUX QUE MON EX"
+            },
+        }]
     }
-    ]
-  }
-  localStorage.writeData(data)
-  localStorage.readData(users)
-  server.receiveDataByXBEE()
+    localStorage.writeData(data)
+    localStorage.readData('users')
+    server.receiveDataByXBEE()
 
 }
