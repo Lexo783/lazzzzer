590bfd14520ebb7b6a3c7b48e33e26284d0affb3
Lexo783 <florentdebuchy@orange.fr>
2022-05-05 14:39:52 +0200

    its not a local its a store

--- socket/src/Services/LocalStore/LocalStore.js
+++ socket/src/Services/LocalStore/LocalStore.js
@@ -1,32 +1,19 @@
-const fs = require('fs');
+var obj = {
 
-module.exports.writeData = function (data) {
-  const json = JSON.stringify(data);
+}
 
-  fs.writeFile('./config.json', json, function (err) {
-    if (err) {
-      console.log('There has been an error saving your configuration data.');
-      console.log(err.message);
-      return;
-    }
-    console.log('Configuration saved successfully.')
-  });
+module.exports.writeData = function (key, data) {
+  if (key in obj) {
+    obj[key] = data
+  } else {
+    obj = data
+  }
 }
 
 module.exports.readData = function (pointer = null) {
-  var data = fs.readFileSync('./config.json'), myObj;
-
-  try {
-    myObj = JSON.parse(data.);
-    console.dir(myObj);
-    if (pointer === null) {
-      return myObj
-    } else {
-      return myObj[pointer]
-    }
-  }
-  catch (err) {
-    console.log('There has been an error parsing your JSON.')
-    console.log(err);
+  if (pointer === null) {
+    return obj[pointer]
+  } else {
+    return null
   }
 }
